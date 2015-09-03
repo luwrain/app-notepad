48d8154f9ac281e449edc4eba054cffc809c750a
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2015-09-03 12:40:12 +0600

    General cleaning and minor fixes
    - Extensions class arrangements
    - i18n.Ru.java goes to i18n.git
    - Copyright header update
    - LICENCE and AUTHORS files added

--- src/main/java/org/luwrain/app/notepad/Extension.java
+++ src/main/java/org/luwrain/app/notepad/Extension.java
@@ -1,25 +1,30 @@
+/*
+   Copyright 2012-2015 Michael Pozhidaev <michael.pozhidaev@gmail.com>
 
+   This file is part of the LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.app.notepad;
 
 import java.util.*;
-
-import org.luwrain.core.Application;
-import org.luwrain.core.Shortcut;
-import org.luwrain.core.Command;
-import org.luwrain.core.CommandEnvironment;
-import org.luwrain.core.Worker;
-import org.luwrain.core.SharedObject;
-import org.luwrain.core.I18nExtension;
-import org.luwrain.core.Luwrain;
-import org.luwrain.core.Registry;
+import org.luwrain.core.*;
 
 public class Extension extends org.luwrain.core.extensions.EmptyExtension
 {
     @Override public Command[] getCommands(Luwrain luwrain)
     {
-	Command[] res = new Command[1];
-	res[0] = new Command(){
+	return new Command[]{
+	    new Command(){
 		@Override public String getName()
 		{
 		    return "notepad";
@@ -28,14 +33,13 @@ public class Extension extends org.luwrain.core.extensions.EmptyExtension
 		{
 		    luwrain.launchApp("notepad");
 		}
-	    };
-	return res;
+	    }};
     }
 
     @Override public Shortcut[] getShortcuts(Luwrain luwrain)
     {
-	Shortcut[] res = new Shortcut[1];
-	res[0] = new Shortcut() {
+	return new Shortcut[]{
+	    new Shortcut() {
 		@Override public String getName()
 		{
 		    return "notepad";
@@ -43,31 +47,15 @@ public class Extension extends org.luwrain.core.extensions.EmptyExtension
 		@Override public Application[] prepareApp(String[] args)
 		{
 		    if (args == null || args.length < 1)
-		    {
-			Application[] res = new Application[1];
-			res[0] = new NotepadApp();
-			return res;
-		    }
-		    LinkedList<Application> v = new LinkedList<Application>();
+			return new Application[]{new NotepadApp()};
+		    final LinkedList<Application> v = new LinkedList<Application>();
 		    for(String s: args)
 			if (s != null)
 			    v.add(new NotepadApp(s));
 		    if (v.isEmpty())
-		    {
-			Application[] res = new Application[1];
-			res[0] = new NotepadApp();
-			return res;
-		    }
+			return new Application[]{new NotepadApp()};
 		    return v.toArray(new Application[v.size()]);
 		}
-	    };
-	return res;
-    }
-
-    @Override public void i18nExtension(Luwrain luwrain, I18nExtension i18nExt)
-    {
-	i18nExt.addCommandTitle("en", "notepad", "Notepad");
-	i18nExt.addCommandTitle("ru", "notepad", "Блокнот");
-	i18nExt.addStrings("ru", NotepadApp.STRINGS_NAME, new org.luwrain.app.notepad.i18n.Ru());
+	    }};
     }
 }
