1e2b5bc9bbf82ae14a910b67a38ce3c0e32a2cde
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-06-23 03:29:43 +0700

    Renaming Area.onEnvironmentEvent() to onSystemEvent()

--- src/main/java/org/luwrain/app/notepad/App.java
+++ src/main/java/org/luwrain/app/notepad/App.java
@@ -104,11 +104,11 @@ class App implements Application
 		    }
 		    return super.onKeyboardEvent(event);
 		}
-		@Override public boolean onEnvironmentEvent(EnvironmentEvent event)
+		@Override public boolean onSystemEvent(EnvironmentEvent event)
 		{
 		    NullCheck.notNull(event, "event");
 		    if (event.getType() != EnvironmentEvent.Type.REGULAR)
-			return super.onEnvironmentEvent(event);
+			return super.onSystemEvent(event);
 		    switch(event.getCode())
 		    {
 		    case CLOSE:
@@ -135,7 +135,7 @@ class App implements Application
 						if (ActionEvent.isAction(event, "run"))
 			    return actions.run(editArea);
 								    default:
-			return super.onEnvironmentEvent(event);
+			return super.onSystemEvent(event);
 		    }
 		}
 		@Override public String getAreaName()
@@ -166,18 +166,18 @@ class App implements Application
 			}
 		    return super.onKeyboardEvent(event);
 		}
-		@Override public boolean onEnvironmentEvent(EnvironmentEvent event)
+		@Override public boolean onSystemEvent(EnvironmentEvent event)
 		{
 		    NullCheck.notNull(event, "event");
 		    if (event.getType() != EnvironmentEvent.Type.REGULAR)
-			return super.onEnvironmentEvent(event);
+			return super.onSystemEvent(event);
 		    switch(event.getCode())
 		    {
 		    case CLOSE:
 			closeApp();
 			return true;
 		    default:
-			return super.onEnvironmentEvent(event);
+			return super.onSystemEvent(event);
 		    }
 		}
 	    };
